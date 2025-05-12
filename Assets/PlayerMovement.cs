e13d41697bb444861ac9161aceb907d076f14f8f
Daniel Gabriel Lazar <danielgabriellazar24@gmail.com>
2025-05-12 20:26:10 +0300

    Ai_Pathfinding

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-
+    public Animator animator;
     public float runSpeed = 40f;
+
+    public float rollSpeed = 20f;
+    public float rollDuration = 0.5f;
+
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
-    // Update is called once per frame
+
+    private bool isRolling = false;
+    private float rollTimer = 0f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
-        if (Input.GetButtonDown("Jump"))
+        // Disable movement input while rolling
+        if (!isRolling)
         {
-            jump = true;
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         }
 
-        if (Input.GetButtonDown("Crouch"))
+        // Jump
+        if (Input.GetButtonDown("Jump") && !isRolling)
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        // Crouch
+        if (Input.GetButtonDown("Crouch") && !isRolling)
         {
             crouch = true;
         }
@@ -28,12 +50,42 @@ public class PlayerMovement : MonoBehaviour
         {
             crouch = false;
         }
+
+        // Roll (Left Shift)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isRolling && controller.IsGrounded())
+        {
+            StartRoll();
+        }
     }
 
-
-    private void FixedUpdate()
+    void StartRoll()
     {
-        // Move our character
+        isRolling = true;
+        rollTimer = rollDuration;
+        animator.SetTrigger("Roll"); // folosim Trigger în loc de SetBool
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        if (isRolling)
+        {
+            float direction = controller.GetFacingDirection(); // +1 sau -1
+            rb.velocity = new Vector2(direction * rollSpeed, rb.velocity.y);
+
+            rollTimer -= Time.fixedDeltaTime;
+            if (rollTimer <= 0f)
+            {
+                isRolling = false;
+            }
+
+            return; // skip normal movement while rolling
+        }
+
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
